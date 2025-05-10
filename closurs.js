8313bc5f4387c302c370fc30f01c2c8ac93e348a
Maruthiprasad12 <pcmaruthiprasad01@gmail.com>
2025-05-10 21:14:55 +0530

    Js code

---
+++ closurs.js
@@ -0,0 +1,25 @@
+function closurs(num){
+    console.log(num)
+    function add(num1,num2){ 
+        console.log(5+2+num) 
+    }
+    return add()
+}
+closurs(3)
+// // function displayname(name){
+// //     function message(){
+// //         console.log("Hii"+' '+name)
+// //     } 
+// //     message();
+// // }
+// // displayname("Maruthi");
+// function greet(name){
+//     let name = "Mani"
+//     function message(){
+//         return "Hii"+' '+ name
+//     }
+//     return message
+// }
+// // const msg=greet()
+// console.log(msg)
+// console.log(msg())
